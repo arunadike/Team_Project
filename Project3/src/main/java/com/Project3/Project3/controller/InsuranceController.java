34596c10efbf01f520e77bb4ee602eeb0c23ab5d
Prithvirajdvn <dvnprithviraj@gmail.com>
2025-04-17 11:36:54 +0530

    Added Insurance Table

---
+++ Project3/src/main/java/com/Project3/Project3/controller/InsuranceController.java
@@ -0,0 +1,31 @@
+package com.Project3.Project3.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Project3.Project3.model.Insurance;
+import com.Project3.Project3.service.InsuranceService;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class InsuranceController {
+	
+	@Autowired
+	InsuranceService insuranceService;
+	
+	@GetMapping("/insuranceGet")
+	public List<Insurance> insuranceGet(){
+		return insuranceService.returnData();
+	}
+	
+	@PostMapping("/insurancePost")
+	public void insurancePost(@RequestBody @Valid Insurance insurance) {
+		insuranceService.saveData(insurance);
+	}
+}
---
+++ Project3/src/main/java/com/Project3/Project3/repository/InsuranceRepository.java
@@ -0,0 +1,10 @@
+package com.Project3.Project3.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Project3.Project3.model.Insurance;
+
+public interface InsuranceRepository extends CrudRepository<Insurance,Integer>{
+
+}
+
